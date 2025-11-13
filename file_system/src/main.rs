27ec467dc64dfe07483fb158c7b428ec29758da1
JoshJD11 <josh.temporal11@gmail.com>
2025-11-12 18:30:59 -0600

    getattr example completed

--- file_system/src/main.rs
+++ file_system/src/main.rs
@@ -5,16 +5,45 @@ use std::path::Path;
 use libc::{ENOENT, ENOSYS};
 // use time::Timespec;
 use libc::timespec;
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
 
 use std::env;
 use fuser::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyData, ReplyEntry, ReplyDirectory};
+
+fn timespec_to_system_time(ts: timespec) -> SystemTime {
+    UNIX_EPOCH + Duration::new(ts.tv_sec as u64, ts.tv_nsec as u32)
+}
 
 struct JsonFilesystem;
 
 impl Filesystem for JsonFilesystem {
     fn getattr(&mut self, _req: &Request, ino: u64, _fh: Option<u64>, reply: ReplyAttr) {
         println!("getattr(ino={})", ino);
-        reply.error(ENOSYS);
+        let ts = timespec_to_system_time(timespec { tv_sec: 0, tv_nsec: 0 }); 
+        let attr = FileAttr {
+            ino: 1,
+            size: 0,
+            blocks: 0,
+            atime: ts,
+            mtime: ts,
+            ctime: ts,
+            crtime: ts,
+            kind: FileType::Directory,
+            perm: 0o755,
+            nlink: 0,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            flags: 0,
+            blksize: 4096 // This is the size of the E/S blocks in bytes that the file system uses for that dir or file.
+        };
+        let ttl = timespec_to_system_time(timespec { tv_sec: 1, tv_nsec: 0 });
+        let ttl_duration = ttl.duration_since(UNIX_EPOCH).unwrap();
+        if ino == 1 {
+            reply.attr(&ttl_duration, &attr);
+        } else {
+            reply.error(ENOSYS);
+        }
     }
 }
 
